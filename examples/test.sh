f4b408c92048c8eaffeef5adcd0c744dd1317f78
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-04-17 01:18:49 -0500

    Make the testing script print nicer messages

--- examples/test.sh
+++ examples/test.sh
@@ -1,3 +1,4 @@
 for i in `ls *.gf`; do
+  echo "\x1b[1m\x1b[37mRunning file $i\n----------------------------------\x1b[0m"
   cat $i | tr -d '\n' | gf --run ../src/MathEng.gf ../src/MathCoq.gf
 done
